7b453b72d8a9fba217b49fd324bafaac867fa791
rudajura <rudolf.jurisica@seznam.cz>
2022-03-05 09:27:59 +0100

    App init

--- calculator.py
+++ calculator.py
@@ -1,1 +1,11 @@
 from tkinter import *
+
+class App:
+    def __init__(self, master):
+        window.title("Calculator")      #jak se bude nase okno jmenovat
+        window.configure(background = "lightgrey")      #nastaveni pozadi okna
+        
+
+window = Tk()
+app = App(window)
+window.mainloop()       #mainloop znaci, ze okno se bude zobrazovat, dokud se nezavre
